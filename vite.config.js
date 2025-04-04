02321937595fe1684a6c3bb03f9eb9149951c758
Matheus Martins <matheus.martins@c3saude.com.br>
2025-04-04 14:51:11 +0000

    Autenticação

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5176, // bate com a porta mapeada no docker-compose
+        strictPort: true, // impede fallback pra outra porta se 5176 estiver ocupada
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
