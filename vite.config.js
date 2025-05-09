d520f0fca52d12f1b8cc76bf2c3d79a48acf019d
Matheus <matheus.martins@c3saude.com.br>
2025-05-09 17:01:50 +0000

    configurações iniciais

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5176, // bate com a porta mapeada no docker-compose
-        strictPort: true, // impede fallback pra outra porta se 5176 estiver ocupada
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
